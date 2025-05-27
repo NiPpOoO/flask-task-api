bfb0bd365bf72c912ba5c87fcf72b755e82e362a
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 17:16:28 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.routes.task_routes import task_bp
 
+app = Flask(__name__)
+app.config.from_object("app.config.Config")
+
+db = SQLAlchemy(app)
+
+app.register_blueprint(task_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
