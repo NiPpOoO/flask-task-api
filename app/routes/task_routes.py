7630b3bb04560961e9121e4ecc4325b2b99706ad
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 17:13:01 +0300

    Update task_routes.py

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,13 @@
+from flask import Blueprint, request, jsonify
+from app.services.task_service import TaskService
 
+task_bp = Blueprint("task", __name__)
+
+@task_bp.route("/tasks", methods=["GET"])
+def get_tasks():
+    return jsonify(TaskService.get_all_tasks())
+
+@task_bp.route("/tasks", methods=["POST"])
+def create_task():
+    data = request.json
+    return jsonify(TaskService.create_task(data["title"], data.get("description", "")))
