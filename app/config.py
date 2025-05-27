52e47e1b24b7ee779daedd65dee0c215046e86d2
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 17:16:11 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+    DEBUG = True
