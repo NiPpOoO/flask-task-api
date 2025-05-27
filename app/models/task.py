466fe651352d70144d715c9617f06b327d2fe411
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 17:12:20 +0300

    Update task.py

--- app/models/task.py
+++ app/models/task.py
@@ -1,1 +1,7 @@
+from app import db
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # Уникальный ID
+    title = db.Column(db.String(255), nullable=False)  # Заголовок задачи
+    description = db.Column(db.Text, nullable=True)  # Описание
+    status = db.Column(db.String(50), default="pending")  # Статус ("pending", "done")
