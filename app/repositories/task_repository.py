b6ed2c433c49e04bdb2ad1ae2e48c5105341065a
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-05-27 17:12:43 +0300

    Update task_repository.py

--- app/repositories/task_repository.py
+++ app/repositories/task_repository.py
@@ -1,1 +1,15 @@
+class TaskRepository:
+    @staticmethod
+    def get_all():
+        return Task.query.all()  # Получить все задачи
 
+    @staticmethod
+    def get_by_id(task_id):
+        return Task.query.get(task_id)  # Найти задачу по ID
+
+    @staticmethod
+    def create(title, description):
+        task = Task(title=title, description=description)
+        db.session.add(task)
+        db.session.commit()
+        return task
